618a335d8d1df3933999b89c25487fef0d39b50c
Loord <15453893522@qq.com>
2025-04-09 21:36:48 +0800

    Site updated: 2025-04-09 21:36:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/番剧网站/","2025/03/30/哲风壁纸/"];function toRandomPost(){
+var posts=["2025/03/30/哲风壁纸/","2025/04/05/Hexo博客部署/","2025/04/04/番剧网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
