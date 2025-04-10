08633b57cc1bf7b2efa39425c41aca54009b1f53
Loord <15453893522@qq.com>
2025-04-10 21:57:30 +0800

    Site updated: 2025-04-10 21:57:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/05/Hexo博客部署/","2025/03/30/哲风壁纸/","2025/04/04/番剧网站/"];function toRandomPost(){
+var posts=["2025/04/09/Hexo博客部署/","2025/04/10/Hexo搭建进阶：Vercel部署、主题安装、基础用法/","2025/03/30/哲风壁纸/","2025/04/04/番剧网站/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
